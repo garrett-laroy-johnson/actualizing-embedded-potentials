fda56f281947e897f8ec66911fd899e6931e5bf6
garrett-laroy-johnson <garrett.laroy.johnson@gmail.com>
2022-10-21 00:04:39 -0500

    fa

--- sketch.js
+++ sketch.js
@@ -1,26 +1,26 @@
 let info = {
   alys: 10,
-  // brandon: 5,
-  // fidelia: 8,
-  // marcel: 10,
-  // megan: 5,
-  // sejo: 5,
-  // shalaka: 10,
-  // teoma: 4,
-  // thembi: 4,
-  // ximena: 6
+  brandon: 5,
+  fidelia: 8,
+  marcel: 10,
+  megan: 5,
+  sejo: 5,
+  shalaka: 10,
+  teoma: 4,
+  thembi: 4,
+  ximena: 6
 }
 let txt = {
   alys: ["Across latitudes and how to build a new, with a new collaborative work Across latitudes and languages", "beginning With care and obsessions", "and letting go of vocabulary constantly", "possibility", "The unforeseen", "Okay blurry", "How to place", "Now relations", "And our differences", "As the as the guiding resource", "So the work", "is more documentation", "Of process", "Then the", "Then the outcome", "The outcome", "would be", "Like", "respectful relationships across", "by artists from different", "geopolitical situations", "and And language that hadn't been seen in advance", "Let the restrictions that house", "house abandon our", "The conventions of outcome focused research proposal to instead focus on"],
-  brandon: ["lorem"],
-  fidelia: ["lorem"],
-  marcel: ["lorem"],
-  megan: ["lorem"],
+  brandon: [" "],
+  fidelia: ["i've been thinking about fans a lot in the last couple of days and paper fans, specifically, which is why i'm Holding This now weeks and folds and the lose and all that fun goodness.yeah there's like that to lose quote that's like i'm forever unfolding between two folds and if to perceive as to if to unfold, then I am forever unfolding between the phone or something like that it's from."],
+  marcel: [""],
+  megan: [""],
   sejo: ["Another project to do as I was saying", "like connecting different like these beautiful rooms and different possible paths between them", "some of them like maybe you could go from one to another direct directly on my way to in order to get to another", "you have to take a longer path"],
-  shalaka: ["lorem"],
-  teoma: ["lorem"],
-  thembi: ["lorem"],
-  ximena: ["lorem"]
+  shalaka: [""],
+  teoma: [""],
+  thembi: [""],
+  ximena: ["i've been dreaming quite a lot with a particular set of colors of sunset so let's say this is a kind of more.", "If there's ethereal.", "So, said, the third area.", "This actually could be the tree.", "This is a physical object that is in location, this is fixing location could be the tree.", "So this is a part of this is this this isn't a location.", "This is something in the imagination.", "This is something that we cannot see.", "And this is the thing that we need to wear this is a wearable, this is a wearable.", "And all these are people who are using that and, of course, I am, you said that I need to do something different, probably i'm doing something similar is quite difficult to to go out but let's say that."]
 }
 let hubs = [];
 let keys;
@@ -28,9 +28,9 @@ let w = window.innerWidth;
 let h = window.innerHeight;
 let ft;
 let txtSize = 30;
+let fonts = ["Dawning of a New Day", "Sacramento", "Waiting for Sunrise"]
 
 function preload() {
-  ft = loadFont("VT323-Regular.ttf")
   // num of get keys;
   keys = Object.keys(info);
   for (k = 0; k < keys.length; k++) {
@@ -48,7 +48,6 @@ function preload() {
 
 function setup() {
   createCanvas(w, keys.length * h);
-  textFont(ft);
   textSize(txtSize);
   textWrap(WORD);
   noFill();
@@ -60,6 +59,7 @@ function setup() {
 function draw() {
   background(255);
   // draw squiggle
+  noFill();
   beginShape();
   curveVertex(hubs[0].x, hubs[0].y)
   for (let hub of hubs) {
@@ -75,26 +75,24 @@ function draw() {
       node.show();
     }
     noStroke();
-    let r = random(1);
+    let r = random(4);
     if (r < 1) {
       fill(255, 100);
       rect(width / 2, hub.y, width / 2, h / 2)
       fill("black");
-      for (t = 0; t < txt[hub.name].length; t++) {
-        text(txt[hub.name][t], width / 2, hub.y + (t * txtSize))
-      }
+      textFont(random(fonts))
+      text(txt[hub.name], width / 2, hub.y, width / 2, hub.y + h)
+    } else if (r < 2) {
+      fill(255, 100);
+      rect(width / 4, hub.y, width / 2, h / 2)
+      fill("black");
+      text(txt[hub.name], width / 4, hub.y, width / 2, h / 2)
+    } else if (r < 3) {
+      fill(255, 100);
+      rect(0, hub.y, width / 2, hub.y + h)
+      fill("black");
+      text(txt[hub.name], 0, hub.y, width / 2, hub.y + h)
     }
-    //else if (r < 2) {
-    //   fill(255, 100);
-    //   rect(width / 4, hub.y, width / 2, h / 2)
-    //   fill("black");
-    //   text(txt[hub.name], width / 4, hub.y, width / 2, h / 2)
-    // } else if (r < 3) {
-    //   fill(255, 100);
-    //   rect(width, hub.y, width / 2, hub.y + h)
-    //   fill("black");
-    //   text(txt[hub.name], width, hub.y, width / 2, hub.y + h)
-    // }
     noLoop();
   }
 }
@@ -104,32 +102,21 @@ class Hub {
       this.index = index,
       this.nodes = [],
       this.x = random(125, w - 125);
-    this.y = (h * this.index) + h / 3;
+    this.y = (h / 2 * this.index) + h / 3;
     this.mass = 1;
   }
 }
 class Node {
   constructor(img, x, y) {
-    // this.des = createVector(random(x + 300, x + 300), random(y - 300, y + 300));
-    this.pos = createVector(random(x, x + 300), random(y - 300, y + 300));
-    // this.vel = p5.Vector.random2D();
-    // this.vel.mult(random(10));
+    this.pos = createVector(random(x, x + 500), random(y - 300, y + 600));
     this.img = img;
     this.r = 250;
   }
   show() {
     this.edges();
+    tint(255, 200)
     image(this.img, this.pos.x, this.pos.y, this.r, this.r)
   }
-  // update(x, y, mag, lim) {
-  //   let hub = createVector(x, y);
-  //   //let ran = p5.Vector.random2D();
-  //   this.acc = p5.Vector.sub(hub, this.pos);
-  //   this.acc.setMag(mag);
-  //   this.vel.add(this.acc);
-  //   this.vel.limit(lim);
-  //   this.pos.add(this.vel);
-  // }
   edges() {
     if (this.pos.x <= 0) {
       this.pos.x = 0;
@@ -138,4 +125,8 @@ class Node {
       this.pos.x = width - 250;
     }
   }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, keys.length * h / 2)
 }
